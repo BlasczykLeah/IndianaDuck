8aa40275a08f13ae79e0094690e697200b4b3d67
WesMcW <wesmcwhorter@gmail.com>
2020-04-24 22:18:02 -0500

    camera rotate dudk

--- IndianaQuack/Assets/Scripts/CameraMove.cs
+++ IndianaQuack/Assets/Scripts/CameraMove.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float xAxiz = 0, yAxiz = 0;
+    public float power = 1;
+
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float rotVert = -Input.GetAxis("Mouse Y");
+        yAxiz += rotVert;
+
+        if(yAxiz > -12 && yAxiz < 12)
+        {
+            transform.RotateAround(transform.position, transform.right, rotVert * power);
+        }
+        else
+        {
+            if (yAxiz < -12)
+                yAxiz = -12;
+            if (yAxiz > 12)
+                yAxiz = 12;
+        }
+
+        float rotHor = -Input.GetAxis("Mouse X");
+        transform.RotateAround(transform.position, -Vector3.up, rotHor * power);
     }
 }
