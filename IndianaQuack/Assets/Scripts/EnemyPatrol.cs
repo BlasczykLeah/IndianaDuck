aad9e6a948c7aadb56de439a622752beb993ef5c
Leah <Lebrra@users.noreply.github.com>
2020-04-24 21:04:08 -0500

    enemy basic patrol works

--- IndianaQuack/Assets/Scripts/EnemyPatrol.cs
+++ IndianaQuack/Assets/Scripts/EnemyPatrol.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(Rigidbody))]
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Patrol Properties")]
+    public Vector3[] movementCorners;    // points of movement
+    public Vector3 playerLocation;
+    public int currentPoint;
+    public bool movementLoops;      // if movement loops, go from last point -> first point, else go from last point -> second last point (reverse order)
+    bool reverse;
+    bool isMoving;
+
+    [Header("Vision Properties")]
+
+    Rigidbody rb;
+    NavMeshAgent agent;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        MoveToPoint(currentPoint);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance == 0 && agent.pathStatus == NavMeshPathStatus.PathComplete && isMoving) // agent.remainingDistance == 0
+        {
+            isMoving = false;
+            UpdateDestination();
+            MoveToPoint(currentPoint);
+        }
+    }
+
+    void MoveToPoint(int pointIndex)
+    {
+        isMoving = true;
+        agent.SetDestination(movementCorners[pointIndex]);
+    }
+
+    void UpdateDestination()
+    {
+        if (!reverse) currentPoint++;
+        else currentPoint--;
+
+        if(currentPoint >= movementCorners.Length || currentPoint < 0)
+        {
+            if (movementLoops) currentPoint = 0;
+            else
+            {
+                if (reverse) currentPoint += 2;
+                else currentPoint -= 2;
+                reverse = !reverse;
+            }
+        }
+    }
+
+    void MoveToPlayer()
+    {
+        if (playerLocation != null) agent.SetDestination(playerLocation);
+        else MoveToPoint(currentPoint);
     }
 }
