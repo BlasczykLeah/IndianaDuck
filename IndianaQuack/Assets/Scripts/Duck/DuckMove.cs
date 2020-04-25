db8bdba94bb7311266128445f5f432203a50ad5d
WesMcW <wesmcwhorter@gmail.com>
2020-04-24 19:53:21 -0500

    move?

---
+++ IndianaQuack/Assets/Scripts/Duck/DuckMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckMove : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float moveSpeed, rotSpeed;
+    private float vert, horz;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Waddle();
+    }
+
+    void Waddle()
+    {
+        vert = Input.GetAxisRaw("Vertical");
+        horz = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = (transform.forward * vert) * moveSpeed * Time.fixedDeltaTime;
+        transform.Rotate((transform.up * horz) * rotSpeed * Time.fixedDeltaTime);
+    }
+}
+
