8891a506cb0180c7c1bd4dfcfd4cb0b6c969e88b
Leah <Lebrra@users.noreply.github.com>
2020-04-25 16:42:44 -0500

    snakessssssssssssssss

---
+++ IndianaQuack/Assets/Editor/QuackRadiusEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(QuackOfCourage))]
+public class QuackRadiusEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        QuackOfCourage quack = (QuackOfCourage)target;
+        Handles.color = Color.yellow;
+        Handles.DrawWireArc(quack.transform.position, Vector3.up, Vector3.forward, 360, quack.quackRadius);
+    }
+}
