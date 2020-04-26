0889d3c6c1d434483f8cffa3ad6026742b894b69
Leah <Lebrra@users.noreply.github.com>
2020-04-25 22:14:32 -0500

    new camera zoom

---
+++ IndianaQuack/Assets/Scripts/BehindCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehindCamera : MonoBehaviour
+{
+    CameraAutoZoom zoom;
+
+    private void Awake()
+    {
+        zoom = transform.parent.GetComponent<CameraAutoZoom>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Enemy") && !other.CompareTag("Player"))
+        {
+            zoom.zoomingOut = true;
+        }
+    }
+}
