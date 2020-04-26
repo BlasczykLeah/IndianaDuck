5617666eedf0e1979a8b4836e868a4daf82ebef1
Leah <Lebrra@users.noreply.github.com>
2020-04-26 17:00:14 -0500

    jhgfr

---
+++ IndianaQuack/Assets/Scripts/CameraCrap/CameraChase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChase : MonoBehaviour
+{
+    //public bool revert;
+    //public bool used;
+
+        public GameObject black;
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !used)
+        {
+            used = true;
+            GameObject temp = other.transform.Find("Camera Holder").gameObject;
+
+            if (revert)
+            {
+                temp.transform.position = Vector3.zero;
+                temp.transform.Rotate(Vector3.zero);
+            }
+            else
+            {
+                temp.transform.position = new Vector3(0, 0, -0.15F);
+                temp.transform.Rotate(new Vector3(0, 180, 0));
+            }
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            black.SetActive(true);
+        }
+    }
+}
